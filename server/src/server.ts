1b3535115ced3f804046ce2981a3115420cbc157
Messenger Clone Developer <rakotomahefamamitiana0-hub@users.noreply.github.com>
2026-09-04 00:39:34 +0200

    Bind server to Render network interface

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,6 @@ import { httpServer } from './app';
 const rawPort = process.env.PORT;
 const parsedPort = rawPort && /^\d+$/.test(rawPort) ? Number(rawPort) : 5000;
 
-httpServer.listen(parsedPort, () => {
+httpServer.listen(parsedPort, '0.0.0.0', () => {
   console.log(`Messenger server is running on http://localhost:${parsedPort}`);
 });
